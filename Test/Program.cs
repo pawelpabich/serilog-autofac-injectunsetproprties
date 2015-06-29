8e65a4b1e85f7e14d500a39a9b307365fcc009b1
Pawel Pabich <pawel@pabich.eu>
2015-06-29 10:06:57 +1000

    SourceContext is not populated

---
+++ Test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Autofac;
+using AutofacSerilogIntegration;
+using Serilog;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            Log.Logger = new LoggerConfiguration()
+                      .WriteTo.Seq("http://localhost:5341")
+                      .CreateLogger();
+
+            builder.RegisterLogger(Log.Logger, true);
+
+            var container = builder.Build();
+            var v = new MyType(container);
+            v.Log();
+            Console.ReadLine();
+        }
+    }
+
+    public class MyType
+    {
+        public MyType(IContainer container)
+        {
+            //Does not populate SourceContext
+            container.InjectUnsetProperties(this);
+        }
+
+        public ILogger Logger { get; set; }
+
+        public void Log()
+        {
+            Logger.Information("Test call {Now}", DateTime.Now);
+        }
+    }
+}
